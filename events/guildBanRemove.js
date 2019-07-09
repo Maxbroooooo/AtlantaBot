9dc46824c6a37cb8e595b836770bbb005aef0f36
Maxbroooooo <49782913+Maxbroooooo@users.noreply.github.com>
2019-07-09 11:15:03 +0200

    Create guildBanRemove.js

---
+++ events/guildBanRemove.js
@@ -0,0 +1,16 @@
+client.on('guildBanRemove', function(guild, user) {
+
+   
+
+    
+    let log = guild.channels.find('name', "logs");
+    if (!log) log = null
+    let embed = new Discord.RichEmbed()
+    .setAuthor(user.username, user.displayAvatarURL)
+    .setDescription(message.language.get("UNBAN_HEADING:"))
+    .setTimestamp()
+    .setColor(this.client.config.embed.color)
+    .setFooter(this.client.config.embed.footer)
+        log.sendMessage(embed);
+
+});
