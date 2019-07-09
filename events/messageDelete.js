2f7202ef1771cf6f4778f1e7408ef164e0add688
Maxbroooooo <49782913+Maxbroooooo@users.noreply.github.com>
2019-07-09 10:56:58 +0200

    Create messageDelete.js

---
+++ events/messageDelete.js
@@ -0,0 +1,20 @@
+  client.on('messageDelete', function(message) {
+
+    if(message.channel.type == 'text') {
+
+        
+        
+        var log = message.guild.channels.find('name', "logs");
+        if (log != null)
+
+        var embed = new Discord.RichEmbed()
+          .setAuthor(message.author.username, message.author.displayAvatarURL)
+          .setDescription("Log des messages \n `DELETE`")
+          .setTimestamp()
+          .addField("Message supprimmé par", message.author, true)
+          .addField("Message:", message.cleanContent, false)
+            log.sendMessage(embed);
+
+    }
+
+});
