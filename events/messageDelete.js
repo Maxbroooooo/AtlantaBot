4cdf34d0069dca5587e4421c6bce5ec9f5941649
Maxbroooooo <49782913+Maxbroooooo@users.noreply.github.com>
2019-07-09 11:39:30 +0200

    Update messageDelete.js

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,18 +1,20 @@
-  client.on('messageDelete', function(message) {
+client.on('messageDelete', function(message) {
 
     if(message.channel.type == 'text') {
 
         
         
-        var log = message.guild.channels.find('name', "logs");
+        let log = message.guild.channels.find('name', "logs");
         if (log != null)
 
-        var embed = new Discord.RichEmbed()
+        let embed = new Discord.RichEmbed()
           .setAuthor(message.author.username, message.author.displayAvatarURL)
-          .setDescription("Log des messages \n `DELETE`")
+          .setDescription(message.language.get("MESSAGE_DELETE")[3])
           .setTimestamp()
-          .addField("Message supprimmé par", message.author, true)
-          .addField("Message:", message.cleanContent, false)
+          .setColor(this.client.config.embed.color)
+          .setFooter(this.client.config.embed.footer)
+          .addField(message.language.get("MESSAGE_DELETE")[0], message.author, true)
+          .addField(message.language.get("MESSAGE_DELETE")[1], message.cleanContent, false)
             log.sendMessage(embed);
 
     }
